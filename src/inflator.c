49ebf690cf92ba880d2a2808b55ed98df7dc73fb
Jpn666 <ngrnjp@gmail.com>
2023-10-31 19:44:18 -0300

    .

--- src/inflator.c
+++ src/inflator.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020, jpn
+ * Copyright (C) 2023, jpn
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
